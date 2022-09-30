44c2b30e9b9b2f3684cb22f0b36e512a93a6c72b
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2022-09-30 13:52:12 +0300

    chore: data import/export test

---
+++ src/components/variables.js
@@ -0,0 +1,33 @@
+export const popupEditProfile = document.querySelector(".popup_edit-profile");
+export const formEditProfile = document.querySelector(".form_edit-profile");
+export const formInputTypeFirstname = document.querySelector(
+  ".form__input_type_firstname"
+);
+export const formInputTypeProfession = document.querySelector(
+  ".form__input_type_profession"
+);
+export const profilTitleFirstname = document.querySelector(
+  ".profile__title-firstname"
+);
+export const profilSubtitleProfession = document.querySelector(
+  ".profile__subtitle-profession"
+);
+export const buttonTypeEdit = document.querySelector(".button_type_edit");
+//_____________________________________________________________________________VARIABLES FOR POPUP ADD
+export const popupAddElement = document.querySelector(".popup_add-elements"); //<-----------------------------Fixed a bug in the variable name (popupAddElements --> popupAddElement)
+export const formInputTypeTown = document.querySelector(".form__input_type_town");
+export const formInputTypeTownLink = document.querySelector(
+  ".form__input_type_townlink"
+);
+// const elementsFoto = document.querySelector(".elements__foto");
+// const elementsTitle = document.querySelector(".elements__title");
+export const buttonTypeAdd = document.querySelector(".button_type_add");
+export const formAddElement = document.querySelector(".form_add-elements"); //<-------------------------------Fixed a bug in the variable name (formAddElements --> formAddElement)
+// const buttonTypeSubmit = document.querySelector(".button_type_submit");<---------------------------The variable has been removed because it is not currently used by the code structure.
+//_____________________________________________________________________________VARIABLES FOR POPUP OVERVIEW
+export const popupOverview = document.querySelector(".popup_overview");
+export const overviewImage = document.querySelector(".overview__image");
+export const overviewCaption = document.querySelector(".overview__caption");
+//_____________________________________________________________________________VARIABLES FOR ELEMENTS
+export const templateElement = document.querySelector("template");
+export const cardsContainer = document.querySelector(".elements");
