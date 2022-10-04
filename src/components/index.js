f8fa0e0517a3e3339e435b65a9fbf94af8a6b5ad
Rustam Nikonchuk <rustam.nikonchuk@yandex.ru>
2022-10-05 01:27:06 +0300

    fixed: layout of popup form elements

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-import '../../src/index.css';
+import "../../src/index.css";
 import {
   formEditProfile,
   buttonTypeEdit,
@@ -23,63 +23,58 @@ import {
   popupEditAvatar,
   avatarLink,
   avatarImage,
-} from './variables.js';
-import { enableValidation, disabledSubmitBtn } from './validate.js';
-import { getElement } from './card.js';
-import { openPopup, closePopup, } from './modal.js';
-import './utils.js';
+} from "./variables.js";
+import { enableValidation, disabledSubmitBtn } from "./validate.js";
+import { getElement } from "./card.js";
+import { openPopup, closePopup } from "./modal.js";
+import "./utils.js";
 
 enableValidation({});
 
 function openformEditProfile() {
-  //the data that will be displayed in the input field
   formInputTypeFirstname.value = profilTitleFirstname.textContent;
   formInputTypeProfession.value = profilSubtitleProfession.textContent;
   openPopup(popupEditProfile);
 }
 
 function closeFormEditProfile(event) {
-  //data that will be sent after
-  event.preventDefault(); //Canceling the default browser action
+  event.preventDefault();
   profilTitleFirstname.textContent = formInputTypeFirstname.value;
   profilSubtitleProfession.textContent = formInputTypeProfession.value;
   closePopup(popupEditProfile);
 }
 
-function openformAddCards(event) {
-  event.preventDefault(); //Canceling the default browser action
+function createСard(event) {
+  event.preventDefault();
   cardsContainer.prepend(
-    //insert nodes or rows at the beginning
-    getElement(formInputTypeTown.value, formInputTypeTownLink.value) //search method
+    getElement(formInputTypeTown.value, formInputTypeTownLink.value)
   );
 
   formAddCard.reset();
   closePopup(popupAddCard);
-  
 }
 
 function openformEditAvatar() {
-  avatarLink.value = avatarImage.getAttribute('src');
   openPopup(popupEditAvatar);
 }
 function closeFormEditAvatar(evt) {
   evt.preventDefault();
   closePopup(popupEditAvatar);
-  avatarImage.setAttribute('src', avatarLink.value);
+  avatarImage.setAttribute("src", avatarLink.value);
 }
 
 export function showImage(event) {
   const image = event.target;
-  overviewImage.setAttribute('src', image.src);
-  overviewImage.setAttribute('alt', image.alt);
+  overviewImage.setAttribute("src", image.src);
+  overviewImage.setAttribute("alt", image.alt);
   overviewCaption.textContent = image.alt;
 
   openPopup(popupOverview);
 }
 
-buttonTypeEdit.addEventListener('click', openformEditProfile);
-formEditProfile.addEventListener('submit', closeFormEditProfile);
-buttonTypeAdd.addEventListener('click', () => openPopup(popupAddCard));
-formAddCard.addEventListener('submit', openformAddCards);
-buttonTypeAvatar.addEventListener('click', openformEditAvatar);
-formEditAvatar.addEventListener('submit', closeFormEditAvatar);
+buttonTypeEdit.addEventListener("click", openformEditProfile);
+formEditProfile.addEventListener("submit", closeFormEditProfile);
+buttonTypeAdd.addEventListener("click", () => openPopup(popupAddCard));
+formAddCard.addEventListener("submit", createСard);
+buttonTypeAvatar.addEventListener("click", openformEditAvatar);
+formEditAvatar.addEventListener("submit", closeFormEditAvatar);
